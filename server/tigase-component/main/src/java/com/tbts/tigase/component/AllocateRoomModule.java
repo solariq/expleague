22a80b8663dd2f2395bb6946af41a0ebe7ba675f
Igor E. Kuralenok <solar@yandex-team.ru>
2015-09-25 13:53:31 +0300

    Server initial commit

---
+++ server/tigase-component/main/src/java/com/tbts/tigase/component/AllocateRoomModule.java
@@ -0,0 +1,44 @@
+package com.tbts.tigase.component;
+
+import com.tbts.tigase.component.com.tbts.experts.ExpertManager;
+import tigase.criteria.Criteria;
+import tigase.criteria.ElementCriteria;
+import tigase.muc.exceptions.MUCException;
+import tigase.muc.modules.GroupchatMessageModule;
+import tigase.server.Message;
+import tigase.server.Packet;
+import tigase.util.TigaseStringprepException;
+import tigase.xml.Element;
+import tigase.xmpp.BareJID;
+import tigase.xmpp.JID;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * User: solar
+ * Date: 23.09.15
+ * Time: 14:46
+ */
+public class AllocateRoomModule extends GroupchatMessageModule {
+  private static final Logger log = Logger.getLogger(AllocateRoomModule.class.getName());
+  Criteria CRIT = ElementCriteria.name("message").add(ElementCriteria.name("subject"));
+
+  @Override
+  public void process(Packet packet) throws MUCException {
+    if (CRIT.match(packet.getElement())) {
+      final BareJID expert = ExpertManager.instance().nextAvailable();
+      try {
+        final Element invite = new Element(Message.ELEM_NAME);
+        final Element x = new Element("x");
+        x.setXMLNS("http://jabber.org/protocol/muc#user");
+        x.addChild(new Element("invite", new String[]{"from"}, new String[]{packet.getStanzaFrom().toString()}));
+        invite.addChild(x);
+        write(Packet.packetInstance(invite, packet.getStanzaTo(), JID.jidInstance(expert, "expert")));
+      } catch (TigaseStringprepException e) {
+        log.log(Level.WARNING, "Error constructing invte", e);
+      }
+    }
+    super.process(packet);
+  }
+}
