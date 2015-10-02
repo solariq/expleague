d23cd0a5ad055682749a4500713a6e86bfe6ca71
moiseyev <alexander@moiseyev.name>
2015-10-02 14:09:12 +0200

    Переделал показ истории на Angular

--- client/cordova/app/history/scripts/IndexController.js
+++ client/cordova/app/history/scripts/IndexController.js
@@ -1,7 +1,19 @@
 angular
   .module('history')
   .controller('IndexController', function($scope, supersonic) {
-    // Controller functionality here
+
+    $scope.showDateTime = function(timestamp) {
+      var monthNames = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+      var date = new Date();
+      date.setTime(timestamp);
+      var day = date.getDate();
+      var month = date.getMonth();
+      var year = date.getFullYear();
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      return day + ' ' + monthNames[month] + ' ' + year + ', ' + hours + ':' + minutes;
+    };
+
     $scope.showResults = function() {
       var modalView = new supersonic.ui.View('history#result');
       var options = {
@@ -9,4 +21,32 @@ angular
       }
       supersonic.ui.modal.show(modalView, options);
     };
+
+    $scope.history = [
+      {
+        id: 3,
+        text: 'Самый большой по площади парк Питера',
+        near: false,
+        expert: false,
+        urgency: 0, // От 0 до 5
+        timestamp: 1443710220000
+      },
+      {
+        id: 2,
+        text: 'Правила парковки в Финляндии',
+        near: false,
+        expert: false,
+        urgency: 0, // От 0 до 5
+        timestamp: 1443565680000
+      },
+      {
+        id: 1,
+        text: 'круглосуточная аптека',
+        near: 'рядом с адресом: Москва, ул. Большая Декабрьская 10',
+        expert: false,
+        urgency: 0, // От 0 до 5
+        timestamp: 1443356040000
+      }
+    ];
+
   });
