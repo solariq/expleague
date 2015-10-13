207f38602e33f0b130a4950133d512b5a7aa912d
moiseyev <alexander@moiseyev.name>
2015-10-14 00:58:21 +0200

    Для экрана заказа переписал код на Angular, добавил валидацию формы

--- client/cordova/app/order/scripts/IndexController.js
+++ client/cordova/app/order/scripts/IndexController.js
@@ -1,5 +1,70 @@
 angular
   .module('order')
   .controller('IndexController', function($scope, supersonic) {
-    // Controller functionality here
+    // Количество денег у пользователя
+    $scope.userBalance = 1000;
+
+    // Массив с названиями интервалов срочности и их ценой
+    $scope.urgencyRange = [
+      {
+        name: 'в течение дня',
+        price: 100
+      },
+      {
+        name: '4 часа',
+        price: 150
+      },
+      {
+        name: '2 часа',
+        price: 200
+      },
+      {
+        name: '1 час',
+        price: 250
+      },
+      {
+        name: 'пол часа',
+        price: 300
+      },
+      {
+        name: 'в течение минуты',
+        price: 350
+      }
+    ];
+
+    // Цена привлечения к поиску эксперта
+    $scope.expertPrice = 100;
+
+    // Начальные значения поисковой формы
+    $scope.order = {
+      text: '',
+      near: false,
+      expert: false,
+      urgency: 2
+    }
+
+    // Возвращает имя выбраного пользователем (текущего) значения urgency
+    $scope.getCurrentUrgencyName = function(){
+      return $scope.urgencyRange[$scope.order.urgency].name;
+    }
+
+    // Общая цена поиска учитывающая срочность и превлечение эксперта
+    $scope.getTotalPrice = function(){
+      var urgencyPrice = $scope.urgencyRange[$scope.order.urgency].price;
+      var expertPrice = $scope.order.expert ? $scope.expertPrice : 0;
+      return urgencyPrice + expertPrice;
+    }
+
+    $scope.submitOrder = function(){
+      // Это всё нужно переделать
+      localStorage.setItem('text', $scope.order.text);
+      localStorage.setItem('speed', $scope.getCurrentUrgencyName());
+      steroids.tabBar.selectTab(1);
+      var modalView = new supersonic.ui.View('history#result');
+      var options = {
+        animate: false
+      }
+      supersonic.ui.modal.show(modalView, options);
+      //defaultFormState();
+    }
   });
