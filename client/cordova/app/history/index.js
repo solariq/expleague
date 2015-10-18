6fa4a473b53860ebe3868a35a2123e1991bd215f
moiseyev <alexander@moiseyev.name>
2015-10-18 11:24:25 +0200

    Удалил код, который был нужен для прототипа

--- client/cordova/app/history/index.js
+++ client/cordova/app/history/index.js
@@ -1,11 +1,4 @@
 angular.module('history', [
   // Declare any module-specific AngularJS dependencies here
-  'common',
-  'ngSanitize'
-]).config([
-  '$compileProvider',
-  function($compileProvider) {
-    // Добавляем схему comgooglemaps к списку стандартных разрешенных схем
-    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|tel|file|comgooglemaps):/);
-  }
+  'common'
 ]);
