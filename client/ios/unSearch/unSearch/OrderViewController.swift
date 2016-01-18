cc33332150610c9227ae74ec0a638cc3d8f0309b
saintnik <saintnik2007@yandex.ru>
2016-01-18 14:31:25 +0300

    Urgency grammar and images fix

--- client/ios/unSearch/unSearch/OrderViewController.swift
+++ client/ios/unSearch/unSearch/OrderViewController.swift
@@ -123,8 +123,8 @@ struct Urgency {
     var type: String;
 
     static let ASAP = Urgency(caption: "срочно", value: 1.0, type: "asap")
-    static let DURING_THE_DAY = Urgency(caption: "в течении дня", value: 0.5, type: "day")
-    static let DURING_THE_WEEK = Urgency(caption: "в течении недели", value: 0.0, type: "week")
+    static let DURING_THE_DAY = Urgency(caption: "в течение дня", value: 0.5, type: "day")
+    static let DURING_THE_WEEK = Urgency(caption: "в течение недели", value: 0.0, type: "week")
 
     static func find(value: Float) -> Urgency {
         if (value < 0.25) {
