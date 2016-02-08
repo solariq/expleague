c44b72784f1d054001392bcd2d974b414ffe5861
solar <ikuralenok@gmail.com>
2016-02-08 15:12:09 +0300

    localhost changed to 0.0.0.0 in XMPP protocol

--- server/akka-xmpp/src/main/java/com/tbts/server/XMPPServer.java
+++ server/akka-xmpp/src/main/java/com/tbts/server/XMPPServer.java
@@ -22,7 +22,7 @@ public class XMPPServer extends UntypedActorAdapter {
   @Override
   public void preStart() throws Exception {
     final ActorRef tcp = Tcp.get(getContext().system()).manager();
-    tcp.tell(TcpMessage.bind(getSelf(), new InetSocketAddress("localhost", 5222), 100, Arrays.asList(TcpSO.keepAlive(true)), false), getSelf());
+    tcp.tell(TcpMessage.bind(getSelf(), new InetSocketAddress("0.0.0.0", 5222), 100, Arrays.asList(TcpSO.keepAlive(true)), false), getSelf());
   }
 
   public void invoke(Tcp.Event msg) {
