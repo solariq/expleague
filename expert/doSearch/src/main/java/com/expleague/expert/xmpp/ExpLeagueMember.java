93bb91401b40c53beead3b2da0c4aad476bccd5b
solar <ikuralenok@gmail.com>
2016-04-14 19:52:49 +0300

    Stupid bug during refactoring

--- expert/doSearch/src/main/java/com/expleague/expert/xmpp/ExpLeagueMember.java
+++ expert/doSearch/src/main/java/com/expleague/expert/xmpp/ExpLeagueMember.java
@@ -4,6 +4,10 @@ import com.expleague.expert.profile.UserProfile;
 import com.expleague.expert.xmpp.events.*;
 import com.expleague.model.Offer;
 import com.expleague.model.Operations;
+import com.expleague.model.Operations.Command;
+import com.expleague.model.Operations.Invite;
+import com.expleague.model.Operations.Progress;
+import com.expleague.model.Operations.Resume;
 import com.expleague.xmpp.Item;
 import com.expleague.xmpp.JID;
 import com.expleague.xmpp.stanza.Iq;
@@ -15,6 +19,8 @@ import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.util.logging.Logger;
+
+import static com.expleague.model.Operations.*;
 
 /**
  * Experts League
@@ -48,35 +54,35 @@ public class ExpLeagueMember extends WeakListenerHolderImpl<ExpertEvent> {
       return;
     if (packet instanceof Message) {
       final Message message = (Message)packet;
-      if (message.has(Offer.class) || message.has(Operations.Command.class)) {
-        if (message.has(Operations.Invite.class) || message.has(Operations.Resume.class)) {
+      if (message.has(Offer.class)) {
+        if (message.has(Invite.class) || message.has(Resume.class)) {
           check = false;
-          if (task == null){
+          if (task == null) {
             Offer offer = message.get(Offer.class);
             try {
               this.task = new ExpertTask(this, this::invoke, profile.allocateTaskDir(offer.room().local()), offer);
               invoke(new TaskStartedEvent(offer, task));
-            }
-            catch (IOException e) {
+            } catch (IOException e) {
               throw new RuntimeException(e);
             }
-            task.processCommand(message.get(Item.class));
-          }
-          else log.severe("New task received while having active: " + task.offer() + " ignoring invitation!");
+            task.processCommand(message.get(Command.class));
+          } else log.severe("New task received while having active: " + task.offer() + " ignoring invitation!");
         }
-        else if (!message.has(Operations.Command.class)){
+        else {
           invoke(new CheckEvent(message));
-          ExpLeagueConnection.instance().send(new Message(system(), new Operations.Ok()));
+          ExpLeagueConnection.instance().send(new Message(system(), new Ok()));
           check = true;
         }
-        else if (task != null) {
+      }
+      else if (message.has(Command.class) || message.has(Progress.class)) {
+        if (task != null) {
           task.processCommand(message.get(Item.class));
         }
-        else if (check && message.has(Operations.Cancel.class)) {
+        else if (check && message.has(Cancel.class)) {
           invoke(new CheckCanceledEvent(message));
         }
         else {
-          log.severe("Command received: "+ message.get(Operations.Command.class) + " while no active tasks exist");
+          log.severe("Command received: " + message.get(Command.class) + " while no active tasks exist");
         }
       }
       else if (task != null) {
