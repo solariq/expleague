4e5a66f254de714c848fbc5748ab9c178a749284
Artem Trofimov <trofimov9artem@gmail.com>
2017-04-27 17:52:46 +0300

    Timeouts instead of syncs in tests

--- server/xmpp-bots/src/test/java/integration_tests/tests/ExpertsAssignmentTest.java
+++ server/xmpp-bots/src/test/java/integration_tests/tests/ExpertsAssignmentTest.java
@@ -14,6 +14,7 @@ import tigase.jaxmpp.core.client.BareJID;
 import tigase.jaxmpp.core.client.exceptions.JaxmppException;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -38,57 +39,73 @@ public class ExpertsAssignmentTest extends BaseRoomTest {
     final BareJID roomJID = obtainRoomWorkState(testName(), clientBot, adminBot, expertBots);
 
     //Act/Assert
-    checkDefaultExpertAnswersAndOthersDoNot(roomJID, defaultExpert, expertBots, clientBot);
+    checkDefaultExpertAnswersAndOthersDoNot(roomJID, defaultExpert, Stream.of(expertBots).filter(expertBot -> !defaultExpert.equals(expertBot)).toArray(ExpertBot[]::new), clientBot);
   }
 
   @Test
-  public void testTaskToSpecifiedExpert() throws JaxmppException {
+  public void testTaskToSpecifiedExpert() throws JaxmppException, InterruptedException {
     checkTaskToSeveralSpecifiedExperts(1, 3);
   }
 
   @Test
-  public void testTaskToSeveralSpecifiedExperts() throws JaxmppException {
+  public void testTaskToSeveralSpecifiedExperts() throws JaxmppException, InterruptedException {
     checkTaskToSeveralSpecifiedExperts(3, 3);
   }
 
-  private void checkTaskToSeveralSpecifiedExperts(int acceptedExpertNum, int extraExpertsNum) throws JaxmppException {
+  @Test
+  public void testTaskToPreferredExpert() throws JaxmppException, InterruptedException {
+    //Arrange
+    final int expertsNum = 4;
+    final ExpertBot[] expertBots = Stream.generate(throwableSupplier(() -> botsManager.nextExpert())).limit(expertsNum).toArray(ExpertBot[]::new);
+    final ExpertBot defaultExpert = expertBots[0];
+    //final ExpertBot secondDefaultExpert = expertBots[1];
+    final AdminBot adminBot = botsManager.nextAdmin();
+    final ClientBot clientBot = botsManager.nextClient();
+
+    final BareJID roomJID = obtainRoomOpenState(testName(), clientBot, adminBot);
+    final Filter expertFilter = new Filter(null, null, Collections.singletonList(JID.parse(defaultExpert.jid().toString())));
+    final Offer offer = new Offer(JID.parse(roomJID.toString()), expertFilter);
+    final ReceivingMessage invite = new ReceivingMessageBuilder().from(roomJID).has(Offer.class).has(Operations.Invite.class).build();
+
+    //Act
+    adminBot.send(roomJID, offer);
+    //Assert
+    checkOtherExpertsAreNotReceivedInvite(roomJID, new ExpertBot[]{defaultExpert}, Stream.of(expertBots).filter(expertBot -> !defaultExpert.equals(expertBot)).toArray(ExpertBot[]::new));
+
+    //Act
+    defaultExpert.sendGroupchat(roomJID, new Operations.Ok());
+    //Assert
+    assertThereAreNoFailedMessages(defaultExpert.tryReceiveMessages(new StateLatch(), invite));
+
+    //Currently, this functionality is changed
+    /*//Act
+    defaultExpert.offline();
+    //Assert
+    checkOtherExpertsAreNotReceivedInvite(roomJID, Stream.of(expertBots).filter(expertBot -> !defaultExpert.equals(expertBot)).toArray(ExpertBot[]::new), new ExpertBot[] {defaultExpert});
+
+    //Act/Assert
+    checkDefaultExpertAnswersAndOthersDoNot(roomJID, secondDefaultExpert, Stream.of(expertBots).filter(expertBot -> !defaultExpert.equals(expertBot) && !secondDefaultExpert.equals(expertBot)).toArray(ExpertBot[]::new), clientBot);*/
+  }
+
+  private void checkTaskToSeveralSpecifiedExperts(int acceptedExpertNum, int extraExpertsNum) throws JaxmppException, InterruptedException {
     //Arrange
     final ExpertBot[] acceptedExperts = Stream.generate(throwableSupplier(() -> botsManager.nextExpert())).limit(acceptedExpertNum).toArray(ExpertBot[]::new);
     final ExpertBot defaultExpert = acceptedExperts[0];
     final ExpertBot[] extraExperts = Stream.generate(throwableSupplier(() -> botsManager.nextExpert())).limit(extraExpertsNum).toArray(ExpertBot[]::new);
     final AdminBot adminBot = botsManager.nextAdmin();
     final ClientBot clientBot = botsManager.nextClient();
-    final BareJID roomJID = obtainRoomOpenState(testName(), clientBot, adminBot);
 
+    final BareJID roomJID = obtainRoomOpenState(testName(), clientBot, adminBot);
     final Filter expertFilter = new Filter(Arrays.stream(acceptedExperts).map(expertBot -> JID.parse(expertBot.jid().toString())).collect(Collectors.toList()), null, null);
     final Offer offer = new Offer(JID.parse(roomJID.toString()), expertFilter);
-    final ReceivingMessageBuilder offerCheck = new ReceivingMessageBuilder().from(domainJID()).has(Offer.class).has(Operations.Check.class);
-    final ReceivingMessageBuilder sync = new ReceivingMessageBuilder().has(Operations.Sync.class);
-    final ReceivingMessageBuilder cancel = new ReceivingMessageBuilder().from(roomJID).has(Operations.Cancel.class);
 
     //Act
     adminBot.send(roomJID, offer);
-    //Assert
-    Arrays.stream(acceptedExperts).forEach(throwableConsumer(expertBot -> assertThereAreNoFailedMessages(expertBot.tryReceiveMessages(new StateLatch(), offerCheck.build()))));
-
-    { //Check other bots are not received invite
-      Arrays.stream(extraExperts).filter(expertBot -> !expertBot.equals(defaultExpert)).forEach(throwableConsumer(expertBot -> {
-        //Act
-        adminBot.send(expertBot.jid(), new Operations.Sync());
-        //Assert
-        assertThereAreNoFailedMessages(expertBot.tryReceiveMessages(new StateLatch(), sync.build()));
-
-        if (expertBot.offerCheckReceivedAndReset()) {
-          //Act
-          expertBot.sendGroupchat(roomJID, new Operations.Ok());
-          //Assert
-          assertThereAreNoFailedMessages(expertBot.tryReceiveMessages(new StateLatch(), cancel.build()));
-        }
-      }));
-    }
+    //Act
+    checkOtherExpertsAreNotReceivedInvite(roomJID, acceptedExperts, extraExperts);
 
     //Act/Assert
-    checkDefaultExpertAnswersAndOthersDoNot(roomJID, defaultExpert, acceptedExperts, clientBot);
+    checkDefaultExpertAnswersAndOthersDoNot(roomJID, defaultExpert, Stream.of(acceptedExperts).filter(expertBot -> !defaultExpert.equals(expertBot)).toArray(ExpertBot[]::new), clientBot);
   }
 
   private void checkDefaultExpertAnswersAndOthersDoNot(BareJID roomJID, ExpertBot defaultExpert, ExpertBot[] expertBots, ClientBot clientBot) throws JaxmppException {
@@ -100,6 +117,7 @@ public class ExpertsAssignmentTest extends BaseRoomTest {
     final ReceivingMessageBuilder cancelOffer = new ReceivingMessageBuilder().from(roomJID).has(Offer.class).has(Operations.Cancel.class);
 
     //Act
+    defaultExpert.sendGroupchat(roomJID, new Operations.Ok());
     Arrays.stream(expertBots).forEach(throwableConsumer(expertBot -> expertBot.sendGroupchat(roomJID, new Operations.Ok())));
     //Assert
     Arrays.stream(expertBots).forEach(throwableConsumer(expertBot -> assertThereAreNoFailedMessages(expertBot.tryReceiveMessages(new StateLatch(), invite.build()))));
@@ -108,11 +126,31 @@ public class ExpertsAssignmentTest extends BaseRoomTest {
     defaultExpert.sendGroupchat(roomJID, new Operations.Start());
     //Assert
     assertThereAreNoFailedMessages(clientBot.tryReceiveMessages(new StateLatch(), startAndExpert));
-    Arrays.stream(expertBots).filter(expertBot -> !expertBot.equals(defaultExpert)).forEach(throwableConsumer(expertBot -> assertThereAreNoFailedMessages(expertBot.tryReceiveMessages(new StateLatch(), cancelOffer.build()))));
+    Arrays.stream(expertBots).forEach(throwableConsumer(expertBot -> assertThereAreNoFailedMessages(expertBot.tryReceiveMessages(new StateLatch(), cancelOffer.build()))));
 
     //Act
     defaultExpert.sendGroupchat(roomJID, answer);
     //Assert
     assertThereAreNoFailedMessages(clientBot.tryReceiveMessages(new StateLatch(), expectedAnswer));
+  }
+
+  private void checkOtherExpertsAreNotReceivedInvite(BareJID roomJID, ExpertBot[] acceptedExperts, ExpertBot[] extraExperts) throws JaxmppException, InterruptedException {
+    //Arrange
+    final ReceivingMessageBuilder offerCheck = new ReceivingMessageBuilder().from(domainJID()).has(Offer.class).has(Operations.Check.class);
+    final ReceivingMessageBuilder cancel = new ReceivingMessageBuilder().from(roomJID).has(Operations.Cancel.class);
+
+    //Assert
+    Arrays.stream(acceptedExperts).forEach(throwableConsumer(expertBot -> assertThereAreNoFailedMessages(expertBot.tryReceiveMessages(new StateLatch(), offerCheck.build()))));
+    Thread.sleep(SYNC_WAIT_TIMEOUT_IN_MILLIS); //wait because sync does not work in this case
+    { //Check other bots are not received invite
+      Arrays.stream(extraExperts).forEach(throwableConsumer(expertBot -> {
+        if (expertBot.offerCheckReceivedAndReset()) {
+          //Act
+          expertBot.sendGroupchat(roomJID, new Operations.Ok());
+          //Assert
+          assertThereAreNoFailedMessages(expertBot.tryReceiveMessages(new StateLatch(), cancel.build()));
+        }
+      }));
+    }
   }
 }
