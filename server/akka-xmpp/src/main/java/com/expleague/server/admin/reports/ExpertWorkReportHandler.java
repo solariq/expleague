de82907804681d903d2af7d86ea5f33d3a4f6350
Artem Trofimov <trofimov9artem@gmail.com>
2017-07-25 23:16:56 +0300

    Report #1 is continue

--- server/akka-xmpp/src/main/java/com/expleague/server/admin/reports/ExpertWorkReportHandler.java
+++ server/akka-xmpp/src/main/java/com/expleague/server/admin/reports/ExpertWorkReportHandler.java
@@ -1,15 +1,30 @@
 package com.expleague.server.admin.reports;
 
+import akka.pattern.Patterns;
+import akka.util.Timeout;
+import com.expleague.model.Answer;
 import com.expleague.model.Offer;
+import com.expleague.model.Operations;
+import com.expleague.server.ExpLeagueServer;
 import com.expleague.server.Roster;
 import com.expleague.server.agents.LaborExchange;
+import com.expleague.server.agents.RoomAgent;
+import com.expleague.server.agents.XMPP;
 import com.expleague.server.dao.sql.MySQLBoard;
 import com.expleague.util.akka.ActorMethod;
+import com.expleague.xmpp.JID;
+import com.expleague.xmpp.stanza.Message;
+import com.expleague.xmpp.stanza.Stanza;
 import org.apache.commons.lang.StringEscapeUtils;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 /**
@@ -27,7 +42,7 @@ public class ExpertWorkReportHandler extends CsvReportHandler {
 
     final MySQLBoard mySQLBoard = (MySQLBoard) board;
     try {
-      headers("timestamp", "expert", "expert rating", "expert status", "topic", "room", "order", "tags", "score");
+      headers("timestamp", "expert", "expert rating", "expert status", "topic", "is continue", "room", "order", "tags", "score");
       final Stream<ResultSet> mainResultStream;
       if (request.expertId() == null)
         mainResultStream = mySQLBoard.stream(
@@ -65,18 +80,42 @@ public class ExpertWorkReportHandler extends CsvReportHandler {
               });
             }
           }
+
+          final String roomId = resultSet.getString(5);
+          final JID jid = JID.parse(roomId + "@muc." + ExpLeagueServer.config().domain());
+          final Timeout timeout = Timeout.apply(Duration.create(10, TimeUnit.MINUTES));
+          final Future<Object> ask = Patterns.ask(XMPP.register(jid, context()), new RoomAgent.DumpRequest(), timeout);
+          //noinspection unchecked
+          final List<Stanza> dump = (List<Stanza>) Await.result(ask, timeout.duration());
+          boolean isContinue = false;
+          { //continue
+            for (final Stanza stanza : dump) {
+              if (stanza instanceof Message && ((Message) stanza).has(Answer.class)) {
+                isContinue = true;
+                break;
+              }
+              else if (stanza instanceof Message && ((Message) stanza).has(Operations.Start.class)) {
+                final Operations.Start start = ((Message) stanza).get(Operations.Start.class);
+                if (start.order().compareTo(orderId) >= 0) {
+                  isContinue = false;
+                  break;
+                }
+              }
+            }
+          }
           row(
               resultSet.getString(1),
               expertId,
               rating,
               resultSet.getString(3),
               topic,
-              resultSet.getString(5),
+              Boolean.toString(isContinue),
+              roomId,
               orderId,
               tags.toString(),
               resultSet.getString(7)
           );
-        } catch (SQLException e) {
+        } catch (Exception e) {
           throw new RuntimeException(e);
         }
       });
