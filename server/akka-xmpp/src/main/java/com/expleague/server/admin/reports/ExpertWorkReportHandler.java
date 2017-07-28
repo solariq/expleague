549b2c7ac0bf064e33ab7dcc992bfd7012fc535d
Artem Trofimov <trofimov9artem@gmail.com>
2017-07-28 18:13:11 +0300

    Report #1 is cancel/done

--- server/akka-xmpp/src/main/java/com/expleague/server/admin/reports/ExpertWorkReportHandler.java
+++ server/akka-xmpp/src/main/java/com/expleague/server/admin/reports/ExpertWorkReportHandler.java
@@ -5,6 +5,7 @@ import akka.util.Timeout;
 import com.expleague.model.Answer;
 import com.expleague.model.Offer;
 import com.expleague.model.Operations;
+import com.expleague.model.OrderState;
 import com.expleague.server.ExpLeagueServer;
 import com.expleague.server.Roster;
 import com.expleague.server.agents.ExpLeagueOrder;
@@ -44,18 +45,18 @@ public class ExpertWorkReportHandler extends CsvReportHandler {
 
     final MySQLBoard mySQLBoard = (MySQLBoard) board;
     try {
-      headers("timestamp", "expert", "expert rating", "expert status", "topic", "continue", "room", "order", "tags", "score");
+      headers("timestamp", "expert", "expert rating", "expert status", "topic", "continue", "room", "order", "tags", "score", "cancel/done");
       final Stream<ResultSet> mainResultStream;
       if (request.expertId() == null)
         mainResultStream = mySQLBoard.stream(
-            "SELECT DISTINCT Participants.timestamp, Participants.partisipant, Users.trusted, Orders.offer, Orders.room, Participants.order, Orders.score, Participants.role FROM Participants INNER JOIN Orders ON Participants.order = Orders.id INNER JOIN Users ON Participants.partisipant = Users.id WHERE Participants.role < 7 AND Participants.timestamp >= ? AND Participants.timestamp <= ? GROUP BY Participants.partisipant, Participants.order, Participants.role ORDER BY Participants.timestamp",
+            "SELECT DISTINCT Participants.timestamp, Participants.partisipant, Users.trusted, Orders.offer, Orders.room, Participants.order, Orders.score, Participants.role FROM Participants INNER JOIN Orders ON Participants.order = Orders.id INNER JOIN Users ON Participants.partisipant = Users.id WHERE Participants.role < 7 AND Participants.timestamp >= ? AND Participants.timestamp <= ? GROUP BY Participants.partisipant, Participants.order ORDER BY Participants.timestamp",
             stmt -> {
               stmt.setTimestamp(1, new Timestamp(request.start()));
               stmt.setTimestamp(2, new Timestamp(request.end()));
             });
       else
         mainResultStream = mySQLBoard.stream(
-            "SELECT DISTINCT Participants.timestamp, Participants.partisipant, Users.trusted, Orders.offer, Orders.room, Participants.order, Orders.score, Participants.role FROM Participants INNER JOIN Orders ON Participants.order = Orders.id INNER JOIN Users ON Participants.partisipant = Users.id WHERE Participants.role < 7 AND Participants.timestamp >= ? AND Participants.timestamp <= ? AND Participants.partisipant = ? GROUP BY Participants.partisipant, Participants.order, Participants.role ORDER BY Participants.timestamp",
+            "SELECT DISTINCT Participants.timestamp, Participants.partisipant, Users.trusted, Orders.offer, Orders.room, Participants.order, Orders.score, Participants.role FROM Participants INNER JOIN Orders ON Participants.order = Orders.id INNER JOIN Users ON Participants.partisipant = Users.id WHERE Participants.role < 7 AND Participants.timestamp >= ? AND Participants.timestamp <= ? AND Participants.partisipant = ? GROUP BY Participants.partisipant, Participants.order ORDER BY Participants.timestamp",
             stmt -> {
               stmt.setTimestamp(1, new Timestamp(request.start()));
               stmt.setTimestamp(2, new Timestamp(request.end()));
@@ -69,6 +70,92 @@ public class ExpertWorkReportHandler extends CsvReportHandler {
           //noinspection ConstantConditions
           final String topic = StringEscapeUtils.escapeCsv(((Offer) Offer.create(resultSet.getString(4))).topic()).replace("\n", "\\n");
           final String orderId = resultSet.getString(6);
+          final String roomId = resultSet.getString(5);
+
+          final JID jid = JID.parse(roomId + "@muc." + ExpLeagueServer.config().domain());
+          final Timeout timeout = Timeout.apply(Duration.create(10, TimeUnit.MINUTES));
+          final Future<Object> ask = Patterns.ask(XMPP.register(jid, context()), new RoomAgent.DumpRequest(), timeout);
+          //noinspection unchecked
+          final List<Stanza> dump = (List<Stanza>) Await.result(ask, timeout.duration());
+          final String owner;
+          try (final PreparedStatement getOwner = mySQLBoard.createStatement("SELECT partisipant FROM Participants WHERE `order` = ? AND role = ?")) {
+            getOwner.setString(1, orderId);
+            getOwner.setInt(2, ExpLeagueOrder.Role.OWNER.index());
+            try (final ResultSet getOwnerResult = getOwner.executeQuery()) {
+              owner = getOwnerResult.next() ? getOwnerResult.getString(1) : null;
+            }
+          }
+
+          String continueText = "";
+          { //continue
+            boolean prevAnswer = false;
+            for (final Stanza stanza : dump) {
+              if (!(stanza instanceof Message))
+                continue;
+              final Message message = ((Message) stanza);
+              if (message.has(Answer.class)) {
+                prevAnswer = true;
+              }
+              else if (message.has(Message.Body.class)) {
+                if (prevAnswer && stanza.from().local().equals(owner)) {
+                  continueText = StringEscapeUtils.escapeCsv(((Message) stanza).get(Message.Body.class).value()).replace("\n", "\\n");
+                  prevAnswer = false;
+                }
+              }
+              else if (message.has(Operations.Start.class)) {
+                final Operations.Start start = ((Message) stanza).get(Operations.Start.class);
+                if (start.order().compareTo(orderId) >= 0) {
+                  break;
+                }
+              }
+            }
+          }
+          final ExpLeagueOrder.Role role = ExpLeagueOrder.Role.valueOf(resultSet.getInt(8));
+          OrderResult orderResult = OrderResult.NA;
+          { //order result
+            if (role == ExpLeagueOrder.Role.DENIER)
+              orderResult = OrderResult.CANCEL_FROM_INVITE;
+            else if (role == ExpLeagueOrder.Role.SLACKER)
+              orderResult = OrderResult.CANCEL_FROM_TASK;
+            else if (role == ExpLeagueOrder.Role.ACTIVE) {
+              boolean started = false;
+              for (final Stanza stanza : dump) {
+                if (!(stanza instanceof Message))
+                  continue;
+                final Message message = ((Message) stanza);
+                if (message.has(Operations.Start.class)) {
+                  final Operations.Start start = (message.get(Operations.Start.class));
+                  if (start.order().equals(orderId)) {
+                    started = true;
+                    orderResult = OrderResult.IN_PROGRESS;
+                  }
+                  else if (started) {
+                    orderResult = OrderResult.NA;
+                    break;
+                  }
+                }
+                else if (started && message.has(Operations.Cancel.class)) {
+                  if (message.from().local().equals(owner)) {
+                    orderResult = OrderResult.CANCEL_BY_CLIENT;
+                    break;
+                  } else if (message.from().local().equals(expertId)) {
+                    orderResult = OrderResult.CANCEL_FROM_TASK;
+                    break;
+                  }
+                }
+                else if (started && message.has(Answer.class) && message.from().local().equals(expertId)) {
+                  orderResult = OrderResult.DONE;
+                  break;
+                } else if (started && message.has(Operations.Progress.class)) {
+                  final Operations.Progress progress = message.get(Operations.Progress.class);
+                  if (orderId.equals(progress.order()) && progress.state() == OrderState.DONE) {
+                    orderResult = OrderResult.DONE;
+                    break;
+                  }
+                }
+              }
+            }
+          }
           final StringBuilder tags = new StringBuilder();
           { //tags
             try (final Stream<ResultSet> tagsStream = mySQLBoard.stream("SELECT Tags.tag FROM Tags INNER JOIN Topics ON Tags.id = Topics.tag WHERE Topics.order = ?",
@@ -83,43 +170,6 @@ public class ExpertWorkReportHandler extends CsvReportHandler {
             }
           }
 
-          final String roomId = resultSet.getString(5);
-          final JID jid = JID.parse(roomId + "@muc." + ExpLeagueServer.config().domain());
-          final Timeout timeout = Timeout.apply(Duration.create(10, TimeUnit.MINUTES));
-          final Future<Object> ask = Patterns.ask(XMPP.register(jid, context()), new RoomAgent.DumpRequest(), timeout);
-          //noinspection unchecked
-          final List<Stanza> dump = (List<Stanza>) Await.result(ask, timeout.duration());
-          String continueText = "";
-          { //continue
-            final String owner;
-            try (final PreparedStatement getOwner = mySQLBoard.createStatement("SELECT partisipant FROM Participants WHERE `order` = ? AND role = ?")) {
-              getOwner.setString(1, orderId);
-              getOwner.setInt(2, ExpLeagueOrder.Role.OWNER.index());
-              try (final ResultSet getOwnerResult = getOwner.executeQuery()) {
-                owner = getOwnerResult.next() ? getOwnerResult.getString(1) : null;
-              }
-            }
-            if (owner != null) {
-              boolean prevAnswer = false;
-              for (final Stanza stanza : dump) {
-                if (stanza instanceof Message && ((Message) stanza).has(Answer.class)) {
-                  prevAnswer = true;
-                }
-                else if (stanza instanceof Message && ((Message) stanza).has(Message.Body.class)) {
-                  if (prevAnswer && stanza.from().local().equals(owner)) {
-                    continueText = StringEscapeUtils.escapeCsv(((Message) stanza).get(Message.Body.class).value()).replace("\n", "\\n");
-                    prevAnswer = false;
-                  }
-                }
-                else if (stanza instanceof Message && ((Message) stanza).has(Operations.Start.class)) {
-                  final Operations.Start start = ((Message) stanza).get(Operations.Start.class);
-                  if (start.order().compareTo(orderId) >= 0) {
-                    break;
-                  }
-                }
-              }
-            }
-          }
           row(
               resultSet.getString(1),
               expertId,
@@ -130,7 +180,8 @@ public class ExpertWorkReportHandler extends CsvReportHandler {
               roomId,
               orderId,
               tags.toString(),
-              resultSet.getString(7)
+              resultSet.getString(7),
+              Integer.toString(orderResult.index())
           );
         } catch (Exception e) {
           throw new RuntimeException(e);
@@ -165,6 +216,25 @@ public class ExpertWorkReportHandler extends CsvReportHandler {
 
     String expertId() {
       return expertId;
+    }
+  }
+
+  private enum OrderResult {
+    CANCEL_FROM_INVITE(0),
+    CANCEL_FROM_TASK(1),
+    DONE(2),
+    CANCEL_BY_CLIENT(3),
+    IN_PROGRESS(4),
+    NA(5),;
+
+    int index;
+
+    OrderResult(int index) {
+      this.index = index;
+    }
+
+    public int index() {
+      return index;
     }
   }
 }
